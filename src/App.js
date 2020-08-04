bf486b4b2b935adfb0c87d32374590e829ab51c0
Johann Montfort <jmonfu@gmail.com>
2020-08-04 21:41:18 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import './App.less';
 import './App.css';
+import Form from './components/Form';
+import CardList from './components/CardList';
 
 class App extends React.Component {
+  state = {
+    profiles: [],
+  };
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
   render() {
-    return (
-      <div className="App">
-        This is the GitHub Cards App
-      </div>
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+    	</div>
     );
-  }
+  }	
 }
 
 export default App;
