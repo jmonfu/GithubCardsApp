ffe14c9a7af8683f7d0165eaa045f37f3ce4f755
Johann Montfort <jmonfu@gmail.com>
2020-08-04 21:18:14 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        This is the GitHub Cards App
+      </div>
+    );
+  }
+}
+
+export default App;
